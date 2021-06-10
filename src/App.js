e6e92c9734d9bb1a5bcab529c3b36ee9e60216fa
root <root@mail.com>
2021-06-10 11:37:35 +0200

    getBalanceOf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import {Web3Context} from 'web3-hooks'
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
+import {ethers} from "ethers"
 import './App.css';
 
 function App() {
   const [web3state, login] = useContext(Web3Context)
+  const [ethBalance, setEthBalance] = useState(0)
+  const [address, setAddress] = useState(ethers.constants.AddressZero)
+
+  const handleClickGetBalance = async () => {
+    const balance = await web3state.provider.getBalance(address)
+    setEthBalance(ethers.utils.formatEther(balance))
+    console.log(balance)
+  }
   return (
     <>
     <p>Metamask installed: {web3state.isMetaMask ? 'yes' : 'no'}</p>
@@ -16,7 +25,16 @@ function App() {
     <p>Network id: {web3state.chainId}</p>
     <p>Network name: {web3state.networkName}</p>
     <p>account: {web3state.account}</p>
-    <p>Network id: {web3state.balance}</p>
+    <p>balance {web3state.balance}</p>
+    <label htmlFor="balanceOf">balance of :</label>
+    <input id="balanceOf" 
+    type="text" 
+    value={address} 
+    placeholder={"ethereum address"} 
+    onChange={(event) => setAddress(event.target.value)}/>
+
+    <button onClick={handleClickGetBalance}>get balance</button>
+    <p>balance of : {address} : {ethBalance} ETH</p>
     </>
   );
 }
